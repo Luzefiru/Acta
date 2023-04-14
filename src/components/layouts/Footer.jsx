92987c680a91d323dffd0074d6cae29f88f73c73
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-14 08:26:19 +0800

    style: refactor Footer component to use BEM CSS naving convention

--- src/components/layouts/Footer.jsx
+++ src/components/layouts/Footer.jsx
@@ -1,29 +1,29 @@
-import "./Footer.css";
-import Logo from "./Logo";
+import './Footer.css';
+import Logo from './Logo';
 
 function Footer() {
   return (
-    <div className="footer">
-      <div className="footer-content">
+    <div className="Footer">
+      <div className="Footer--wrapper">
         <Logo color="var(--white)" hoverColor="var(--white)" />
-        <div className="product">
+        <div className="Footer__product">
           <h3>Product</h3>
           <a>All Jobs</a>
           <a>Companies</a>
           <a>Candidates</a>
         </div>
-        <div className="resources">
+        <div className="Footer__resources">
           <h3>Resources</h3>
           <a>Blog</a>
           <a>User guides</a>
         </div>
-        <div className="company">
+        <div className="Footer__company">
           <h3>Company</h3>
           <a>About</a>
           <a>Join us</a>
         </div>
       </div>
-      <div className="copyright">
+      <div className="Footer__copyright">
         <p>© 2023 Acta</p>
       </div>
     </div>
