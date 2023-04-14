2ea262b68a888df66842732dd575dfc2b7bbda78
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-14 23:26:20 +0800

    docs: add sample data set for filterPosts

--- src/utils/filterPosts.js
+++ src/utils/filterPosts.js
@@ -15,3 +15,16 @@ const filterPosts = (() => {
 })();
 
 export default filterPosts;
+
+/* Sample Array Element
+
+{
+  postID: '0000001',
+  authorUID: '87cxzids989a',
+  datePosted: 1681354006526,
+  title: 'Functional Versus Object-Oriented Programming',
+  body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam quidem praesentium deleniti illum totam omnis expedita explicabo dolores. Quidem, minus accusamus earum temporibus laudantium dolorum, iusto quas voluptas neque facere id tempora possimus ea harum rerum dolor culpa inventore, est repellendus voluptatibus similique iure. Rerum odio beatae nulla assumenda consequatur!',
+  tags: ['Programming', 'Computer Science', 'Technology'],
+}
+
+*/
