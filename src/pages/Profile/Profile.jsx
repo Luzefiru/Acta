5dffea294d629d599ef8355da8601e5f7dafa910
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-14 15:34:56 +0800

    feat: add Profile page header, tabs, and ProfileCard

--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,7 +1,19 @@
 import './Profile.css';
+import ProfileCard from './ProfileCard';
 
 function Profile() {
-  return <div className="Profile">I'm in Profile!</div>;
+  return (
+    <div className="Profile">
+      <h1 className="Profile__heading">Settings</h1>
+      <nav className="Profile__tabs">
+        <ul className="Profile__tabs--ul">
+          <li className="Profile__tabs__header active">General</li>
+          <li className="Profile__tabs__header">Account</li>
+        </ul>
+      </nav>
+      <ProfileCard />
+    </div>
+  );
 }
 
 export default Profile;
