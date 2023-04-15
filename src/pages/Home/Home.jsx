b3e9a16601a3dee2a96fcdd2636c3ac54a218703
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 18:19:34 +0800

    feat: finish landing page basic design with responsiveness

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,26 @@
-import ContainedButton from '../../components/ui/ContainedButton.jsx';
-import GhostButton from '../../components/ui/GhostButton.jsx';
+import './Home.css';
+import HeroIllustration from '../../assets/hero-illustration.svg';
+import ContainedButton from '../../components/ui/ContainedButton';
 
 function Home() {
   return (
     <div className="Home">
-      I'm in Home!
-      <ContainedButton content="Edit" />
-      <GhostButton content="Edit" />
+      <main className="Home__hero">
+        <img
+          src={HeroIllustration}
+          alt="Bloggers Creating a Page"
+          class="Home__hero__img"
+        />
+        <div class="Home__hero__headline">
+          Empower Your Blogging Journey with Acta
+          <div class="Home__hero__headline__subtext">
+            So what are you waiting for? Sign up for Acta today and start
+            sharing your ideas with the world! It's quick, easy, and completely
+            free - and who knows where it could take you?
+          </div>
+          <ContainedButton padding="14px 24px">Sign Up Now</ContainedButton>
+        </div>
+      </main>
     </div>
   );
 }
