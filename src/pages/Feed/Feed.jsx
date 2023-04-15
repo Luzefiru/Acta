3867d45ebd692efff7ff9daefd3b58d74a1855ab
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 12:04:41 +0800

    feat: finalize sidebar news cards

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -9,11 +9,34 @@ function Feed() {
       </main>
       <aside className="Feed__aside">
         <h2 className="Feed__aside header">News</h2>
-        <CardNews
-          imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
-          header="ChatGPT Surpasses Human Intelligence"
-          datePosted="Today"
-        />
+        <div className="Feed__aside__cards">
+          {' '}
+          <CardNews
+            imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            header="ChatGPT Surpasses Human Intelligence"
+            datePosted="Today"
+          />
+          <CardNews
+            imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            header="ChatGPT Surpasses Human Intelligence"
+            datePosted="Today"
+          />
+          <CardNews
+            imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            header="ChatGPT Surpasses Human Intelligence"
+            datePosted="Today"
+          />
+          <CardNews
+            imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            header="ChatGPT Surpasses Human Intelligence"
+            datePosted="Today"
+          />
+          <CardNews
+            imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+            header="ChatGPT Surpasses Human Intelligence"
+            datePosted="Today"
+          />
+        </div>
       </aside>
     </div>
   );
