ccfce03964a8c428732960791c1534099156edab
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 11:53:37 +0800

    feat: finalize static news card layout

--- src/pages/Feed/CardNews.jsx
+++ src/pages/Feed/CardNews.jsx
@@ -1,19 +1,14 @@
 import './CardNews.css';
-import ArticleTag from '../../components/ui/ArticleTag';
 
 function CardNews(props) {
-  const { imgSrc, header, tags } = props;
+  const { imgSrc, header, datePosted } = props;
 
   return (
     <article className="CardNews">
       <img className="CardNews__img" src={imgSrc} alt="News Card" />
       <div className="CardNews__details">
         <h4 className="CardNews__details__header">{header}</h4>
-        <div className="CardNews__details__header">
-          {tags.map((tag) => (
-            <ArticleTag>{tag}</ArticleTag>
-          ))}
-        </div>
+        <div className="CardNews__details__date">{datePosted}</div>
       </div>
     </article>
   );
--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -1,4 +1,5 @@
 import './Feed.css';
+import CardNews from './CardNews';
 
 function Feed() {
   return (
@@ -8,6 +9,11 @@ function Feed() {
       </main>
       <aside className="Feed__aside">
         <h2 className="Feed__aside__header">News</h2>
+        <CardNews
+          imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
+          header="ChatGPT Surpasses Human Intelligence"
+          datePosted="Today"
+        />
       </aside>
     </div>
   );
