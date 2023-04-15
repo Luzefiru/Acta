0b87d80719c1d345f48193c15dd9e0119b271be7
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 09:42:10 +0800

    fix: stop ProfileCard from growing with Profile tab components

--- src/pages/Profile/TabGeneral.jsx
+++ src/pages/Profile/TabGeneral.jsx
@@ -32,22 +32,16 @@ function TabGeneral() {
       <h2 className="TabGeneral__header">Account</h2>
       <fieldset className="TabGeneral__fieldset profile">
         <TextField
-          className="TabGeneral__fieldset__fullName"
-          id="fullName"
-          label="Full Name"
-          placeholder="Your full name"
+          className="TabGeneral__fieldset__email"
+          id="email"
+          label="Email"
+          placeholder="example.email@gmail.com"
         />
         <TextField
-          className="TabGeneral__fieldset__displayName"
-          id="displayName"
-          label="Display Name"
-          placeholder="Your display name"
-        />
-        <TextArea
-          className="TabGeneral__fieldset__bio"
-          id="bio"
-          label="Bio"
-          placeholder="Say something about yourself"
+          className="TabGeneral__fieldset__password"
+          id="password"
+          label="Password"
+          placeholder="●●●●●●●●"
         />
       </fieldset>
     </form>
