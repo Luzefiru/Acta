ab39cad49973924c52deb6a2625c207ebe5fc4c6
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 16:16:26 +0800

    feat: add wave animation to auth screen

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,10 +1,16 @@
 import './Auth.css';
 import { Outlet } from 'react-router-dom';
+import WaveBackground from '../../assets/waves.svg';
 
 function Auth() {
   return (
     <div className="Auth">
       <Outlet />
+      <img
+        className="Auth__waves"
+        alt="Animated Waves"
+        src={WaveBackground}
+      ></img>
     </div>
   );
 }
