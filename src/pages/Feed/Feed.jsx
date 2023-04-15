f5a80406bb5b5c98351bd038c106dda07e377cb3
Luzefiru <ian.deejay.iv@gmail.com>
2023-04-15 12:01:23 +0800

    feat: add responsive typography to CardNews

--- src/pages/Feed/Feed.jsx
+++ src/pages/Feed/Feed.jsx
@@ -5,10 +5,10 @@ function Feed() {
   return (
     <div className="Feed">
       <main className="Feed__main">
-        <h2 className="Feed__main__header">Latest Articles</h2>
+        <h2 className="Feed__main header">Latest Articles</h2>
       </main>
       <aside className="Feed__aside">
-        <h2 className="Feed__aside__header">News</h2>
+        <h2 className="Feed__aside header">News</h2>
         <CardNews
           imgSrc="https://images.unsplash.com/photo-1489100517551-92a468b736f0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1632&q=80"
           header="ChatGPT Surpasses Human Intelligence"
