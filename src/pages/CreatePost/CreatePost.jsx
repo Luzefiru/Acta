4c6e588fe042d8b2da79a405809f5bdf8461c08c
kulaizki <kulaizke@gmail.com>
2023-04-16 14:31:47 +0800

    feat: add removable tags and repositioned card

--- src/pages/CreatePost/CreatePost.jsx
+++ src/pages/CreatePost/CreatePost.jsx
@@ -1,4 +1,5 @@
 import "./CreatePost.css";
+import AddedTags from "./AddedTags";
 import TextField from "../../components/forms/TextField";
 import TextArea from "../../components/forms/TextArea";
 import GhostButton from "../../components/ui/GhostButton";
@@ -9,8 +10,25 @@ function CreatePost() {
   return (
     <form className="CreatePost">
       <h1>Create a New Article</h1>
-      <div className="CreatePost__articleDetails">
-        <h3>Article Details</h3>
+      <div className="CreatePost__card">
+        <div className="CreatePost__articleDetails">
+          <h3>Article Details</h3>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="16"
+            height="16"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinejoin="round"
+            strokeLinecap="round"
+            className="lucide lucide-chevron-down"
+          >
+            <polyline points="6 9 12 15 18 9"></polyline>
+          </svg>
+        </div>
+
         <fieldset className="CreatePost__fieldset">
           <TextField
             className="CreatePost__fieldset__title"
@@ -27,13 +45,14 @@ function CreatePost() {
         </fieldset>
         <div className="CreatePost__tags">
           <h4>Tags</h4>
-          <div className="CreatePost__addedTags"></div>
+          <div className="CreatePost__addedTags">
+            <AddedTags />
+          </div>
           <button className="CreatePost__addTag-btn">+ Add another tag</button>
         </div>
         <div className="CreatePost__mainButtons">
           <GhostButton content="Cancel" />
-          <GhostButton content="Save" />
-          <ContainedButton>Save and Publish</ContainedButton>
+          <ContainedButton>Post</ContainedButton>
         </div>
       </div>
       <img
