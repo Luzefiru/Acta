fbb8ac2f668afc93f8e6c6078456e773c5e5a269
Luzefiru <ian.deejay.iv@gmail.com>
2023-05-16 23:02:09 +0800

    feat: make navbar profile picture redirect to proper uid

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -128,7 +128,7 @@ function displayNav(expandedNavItems, user) {
           <line x1="21" x2="9" y1="12" y2="12"></line>
         </svg>
       </Link>
-      <Link className="Navbar__profile-photo" to="/profile/test">
+      <Link className="Navbar__profile-photo" to={`/profile/${user.uid}`}>
         <img
           className="Navbar__profile-photo__img"
           alt="User Profile"
@@ -144,76 +144,78 @@ function displayNav(expandedNavItems, user) {
         </span>
       </Link>
 
-      {expandedNavItems}
+      {expandedNavItems(user)}
     </nav>
   );
 }
 
-var expandedNavItems = (
-  <div className="expandedNavItems">
-    <div className="expandedNavItems__menu-title">Navigation</div>
-    <Link to="/feed">
-      <svg
-        className="Navbar__links__feed-icon lucide lucide-newspaper"
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
+function expandedNavItems(user) {
+  return (
+    <div className="expandedNavItems">
+      <div className="expandedNavItems__menu-title">Navigation</div>
+      <Link to="/feed">
+        <svg
+          className="Navbar__links__feed-icon lucide lucide-newspaper"
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2"></path>
+          <path d="M18 14h-8"></path>
+          <path d="M15 18h-5"></path>
+          <path d="M10 6h8v4h-8V6Z"></path>
+        </svg>
+        Feed
+      </Link>
+      <Link to={`/profile/${user.uid}`}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          class="lucide lucide-user"
+        >
+          <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
+          <circle cx="12" cy="7" r="4"></circle>
+        </svg>
+        Profile
+      </Link>
+      <Link
+        onClick={() => {
+          signOut(auth);
+        }}
+        className="Link--logout"
+        to="/"
       >
-        <path d="M4 22h16a2 2 0 0 0 2-2V4a2 2 0 0 0-2-2H8a2 2 0 0 0-2 2v16a2 2 0 0 1-2 2Zm0 0a2 2 0 0 1-2-2v-9c0-1.1.9-2 2-2h2"></path>
-        <path d="M18 14h-8"></path>
-        <path d="M15 18h-5"></path>
-        <path d="M10 6h8v4h-8V6Z"></path>
-      </svg>
-      Feed
-    </Link>
-    <Link to="/profile/test">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        class="lucide lucide-user"
-      >
-        <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"></path>
-        <circle cx="12" cy="7" r="4"></circle>
-      </svg>
-      Profile
-    </Link>
-    <Link
-      onClick={() => {
-        signOut(auth);
-      }}
-      className="Link--logout"
-      to="/"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="22"
-        height="22"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        class="lucide lucide-log-out"
-      >
-        <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
-        <polyline points="16 17 21 12 16 7"></polyline>
-        <line x1="21" x2="9" y1="12" y2="12"></line>
-      </svg>
-      Log Out
-    </Link>
-  </div>
-);
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="22"
+          height="22"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          class="lucide lucide-log-out"
+        >
+          <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path>
+          <polyline points="16 17 21 12 16 7"></polyline>
+          <line x1="21" x2="9" y1="12" y2="12"></line>
+        </svg>
+        Log Out
+      </Link>
+    </div>
+  );
+}
